5698281a75f68b021bdf2da28e94504f00a3404c
深山　俊 <macnel@fukayamashunnoMacBook-Pro.local>
2020-11-24 22:49:21 +0900

    tag-model,controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'users#home'
   get 'home' => 'users#home'
+  resources :users
+  resources :tags, only: [:index, :show, :edit, :update, :new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
